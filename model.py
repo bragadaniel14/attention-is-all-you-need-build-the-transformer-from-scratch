df2030ae603adf7c4618031cdd4e01face0b0c2c
bragadaniel14 <37494177+bragadaniel14@users.noreply.github.com>
2026-09-21 11:44:41 -0400

    Step 2: build_id_to_token_vocab

--- model.py
+++ model.py
@@ -21,10 +21,7 @@ def build_token_to_id_vocab(sentences, specials=('<pad>', '<bos>', '<eos>', '<un
 # Step 2 - build_id_to_token_vocab
 def build_id_to_token_vocab(token_to_id):
     # TODO: build the inverse id-to-token dictionary from token_to_id
-    id_to_token = {}
-    for token in token_to_id:
-        id_to_token[token_to_id[token]] = token
-    return id_to_token
+    return {idx:tok for tok,idx in token_to_id.items()}
 
 # Step 3 - encode_sentence_to_ids
 def encode_sentence_to_ids(sentence, token_to_id, unk_token='<unk>'):
